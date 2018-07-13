693aed08fec985eb6e8fd06c1ba09f44402e64c3
Wadhwa Akash <10057286@gemalto.com>
2018-07-13 14:13:38 +0530

    first commit

---
+++ src/com/sample/SampleClass.java
@@ -0,0 +1,11 @@
+package com.sample;
+import org.testng.annotations.Test;
+
+public class SampleClass {
+	
+	@Test
+	public void show(){
+		System.out.println("Akash");
+	}
+
+}
---
+++ src/com/sample/SampleClass1.java
@@ -0,0 +1,11 @@
+package com.sample;
+import org.testng.annotations.Test;
+
+public class SampleClass1 {
+	
+	@Test
+	public void show(){
+		System.out.println("Akash Wadhwa");
+	}
+
+}
